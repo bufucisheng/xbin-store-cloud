74d0a0f2ecccb31c98c253daab7bf02bceb140a5
bufucisheng <bu_fucisheng@163.com>
2018-03-19 17:19:27 +0800

    2018.3.19号提交

---
+++ xbin-store-cloud-master/xbin-store-cloud-web-sso/src/main/java/cn/binux/XbinStoreWebSSOApplication.java
@@ -0,0 +1,30 @@
+package cn.binux;
+
+import cn.binux.utils.JedisClient;
+import cn.binux.utils.impl.JedisClientSingle;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableFeignClients
+@Configuration
+//@EnableApolloConfig
+@EnableHystrix
+public class XbinStoreWebSSOApplication  {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XbinStoreWebSSOApplication.class, args);
+	}
+
+	@Bean
+	public JedisClient jedisClient() {
+		return new JedisClientSingle();
+	}
+}
